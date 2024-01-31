1d753fdc8e590816249a7e3de63a1272c77633f4
sly08051980 <regnier.sylvain@yahoo.Fr>
2024-01-31 19:53:03 +0100

    antoine

--- src/Controller/PersonController.php
+++ src/Controller/PersonController.php
@@ -4,15 +4,16 @@ namespace App\Controller;
 
 use App\Entity\Person;
 use App\Repository\PersonRepository;
-use App\Form\UserType;
+use App\Form\PersonType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PersonController extends AbstractController
 {
-    #[Route('/person', name: 'app_person')]
+    #[Route('/person/', name: 'app_person')]
     public function index(): Response
     {
         return $this->render('person/index.html.twig', [
@@ -20,93 +21,129 @@ class PersonController extends AbstractController
         ]);
     }
 
-    #[Route('/create_person', name: 'create_person')]
-    public function createPerson(EntityManagerInterface $entityManager): Response
-    {
-        $date_anniversaire = new \DateTime('10-08-2013');
+
+    // TODO : Voir doc : https://symfony.com/doc/current/forms.html
+    #[Route('/person/create', name: 'create_person')]
+    public function createPerson(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response {
+        // $date_anniversaire = new \DateTime('10-08-2013');
+        // $person = new Person();
+        // $person->setNom('regnier');
+        // $person->setPrenom('sylvain');
+        // $person->setVille('Marseille');
+        // $person->setAge('20');
+        // $person->setDateAnniversaire($date_anniversaire);
+
+        // //persist prepare les donnée de l objet ds l orm
+
+        // $entityManager->persist($person);
+
+        // // flush va inserer les donnée ds la bdd
+
+        // $entityManager->flush();
+
+       // return new Response('Saved new person with id ' . $person->getId());
         $person = new Person();
-        $person->setNom('regnier');
-        $person->setPrenom('sylvain');
-        $person->setVille('Marseille');
-        $person->setAge('20');
-        $person->setDateAnniversaire($date_anniversaire);
 
-        //persist prepare les donnée de l objet ds l orm
+        $form = $this->createForm(PersonType::class, $person);
+      
+        $form->handleRequest($request);
+       
+        if ($form->isSubmitted() && $form->isValid()) {
+            $person = $form->getData();
 
-        $entityManager->persist($person);
+            $entityManager->persist($person);
+            $entityManager->flush();
 
-        // flush va inserer les donnée ds la bdd
 
-        $entityManager->flush();
+            return $this->redirectToRoute('list_person');
+        }
 
-        return new Response('Saved new person with id ' . $person->getId());
+       return $this->render('person/create.html.twig', [
+        'form' => $form->createView()
+    ]);
+
 
     }
 
-    #[Route('/persons', name: 'app_all_persons', methods: ['GET'])]
+    #[Route('/persons/list', name: 'list_person', methods: ['GET'])]
 
-    public function all_persons(PersonRepository $personRepository): Response
+    public function personList(PersonRepository $personRepository): Response
     {
         return $this->render('person/all_persons.html.twig', ['persons' => $personRepository->findAll()]);
-
     }
-    #[Route('/person/{id}', name: 'person_show', methods: ['GET'])]
 
-    public function person_show(int $id, PersonRepository $personRepository): Response
+    #[Route('/person/show/{id}', name: 'person_show', methods: ['GET'])]
+    public function personShow(int $id, PersonRepository $personRepository): Response
     {
         $person = $personRepository->find($id);
 
         if (!$person) {
             throw $this->createNotFoundException('pas de personne' . $id);
         }
-        return $this->render('person/show.html.twig', ['person' => $personRepository->find($id)]);
+
+        $form = $this->createForm(UserType::class, $person);
+
+        return $this->render('person/show.html.twig', [
+            'person' => $personRepository->find($id),
+            'form' => $form->createView()
+        
+        
+        ]);
     }
+
+
 
 
     #[Route('/personne/{id}/delete', name: 'person_delete')]
     public function delete(int $id, Person $personne, EntityManagerInterface $entityManager): Response
-{
+    {
 
- $entityManager->remove($personne);
- $entityManager->flush();
+        $entityManager->remove($personne);
+        $entityManager->flush();
 
- return $this->redirectToRoute('app_all_persons', [], Response::HTTP_SEE_OTHER);
-}
-#[Route('/person/{id}/edit/request',name:'person_edit')]
-public function editRequest(int $id,PersonRepository $personRepository,EntityManagerInterface $entityManager){
-    $person = $personRepository->find($id);
-    $person->setNom($_POST['nom']);
-    $person->setPrenom($_POST['prenom']);
-    $person->setVille($_POST['ville']);
-    $person->setAge($_POST['age']);
-    $person->setDateAnniversaire(new \DateTime($_POST['date_anniversaire']));
+        return $this->redirectToRoute('app_all_persons', [], Response::HTTP_SEE_OTHER);
+    }
+    #[Route('/person/{id}/edit/request', name: 'person_edit')]
+    public function editRequest(int $id, PersonRepository $personRepository, EntityManagerInterface $entityManager)
+    {
+        $person = $personRepository->find($id);
+        $person->setNom($_POST['nom']);
+        $person->setPrenom($_POST['prenom']);
+        $person->setVille($_POST['ville']);
+        $person->setAge($_POST['age']);
+        $person->setDateAnniversaire(new \DateTime($_POST['date_anniversaire']));
 
-    $entityManager->persist($person);
-    $entityManager->flush;
-    return $this->redirectToRoute('app_all_persons',[],Response::HTTP_SEE_OTHER);
-}
-// #[Route('/form', name: 'form_user')]
-// public function forme(): Response
-// {
-// $form = $this->createForm(UserType::class);
-// return $this->render('user/formulaire.html.twig', [
-// 'form' => $form->createView()
-// ]);
-// }
+        $entityManager->persist($person);
+        $entityManager->flush;
+        return $this->redirectToRoute('app_all_persons', [], Response::HTTP_SEE_OTHER);
+    }
+    // #[Route('/form', name: 'form_user')]
+    // public function forme(): Response
+    // {
+    // $form = $this->createForm(UserType::class);
+    // return $this->render('user/formulaire.html.twig', [
+    // 'form' => $form->createView()
+    // ]);
+    // }
 
-#[Route('/form', name:'form')]
+
+
+
+    #[Route('/form', name: 'form')]
     public function form(Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) 
-                {
-                    $entityManager->persist($user);
-                    $entityManager->flush();
-                }
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
         return $this->render('/user/formulaire.html.twig', [
-            'form'=> $form->createView(),
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/Form/PersonType.php
+++ src/Form/PersonType.php
@@ -4,19 +4,30 @@ namespace App\Form;
 
 use App\Entity\Person;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserType extends AbstractType
+class PersonType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
+            ->add('nom', TextType::class, [
+                "attr" => [
+                  
+                ]
+            ])
             ->add('prenom')
             ->add('ville')
             ->add('age')
             ->add('date_anniversaire')
+            ->add('button', SubmitType::class, [
+                'label' => 'Sauvegarder'
+            ])
         ;
     }
 
